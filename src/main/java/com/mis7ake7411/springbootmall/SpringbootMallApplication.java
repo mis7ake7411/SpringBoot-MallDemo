f2f09eddcb6eb90604836e1acba1d419187779d5
mis7ake7411 <feng.school@gmail.com>
2024-07-03 12:44:16 +0800

    初始化

---
+++ src/main/java/com/mis7ake7411/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.mis7ake7411.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mis7ake7411/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis7ake7411.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
