ec27573801a84db3b81d3ecdff8d7536c9d562d5
mis7ake7411 <feng.school@gmail.com>
2024-07-05 15:36:11 +0800

    實作查詢商品列表(分頁)

--- src/main/java/com/mis7ake7411/springbootmall/controller/ProductController.java
+++ src/main/java/com/mis7ake7411/springbootmall/controller/ProductController.java
@@ -5,74 +5,86 @@ import com.mis7ake7411.springbootmall.dto.ProductDto;
 import com.mis7ake7411.springbootmall.dto.ProductQueryParams;
 import com.mis7ake7411.springbootmall.model.Product;
 import com.mis7ake7411.springbootmall.service.ProductService;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
-    @Autowired
-    ProductService productService;
+  @Autowired
+  ProductService productService;
 
-    @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(@RequestParam(required = false) ProductCategory category,
-                                                     @RequestParam(required = false) String search,
-                                                     @RequestParam(required = false) String[] orderBy,
-                                                     @RequestParam(required = false) String[] sort) {
-        ProductQueryParams queryParams = ProductQueryParams.builder()
-                                                           .category(category)
-                                                           .search(search)
-                                                           .orderBy(orderBy)
-                                                           .sort(sort)
-                                                           .build();
-        List<Product> list = productService.getProducts(queryParams);
-        return ResponseEntity.status(HttpStatus.OK)
-                             .body(list);
+  @GetMapping("/products")
+  public ResponseEntity<List<Product>> getProducts(
+      // 查詢條件 Filtering
+      @RequestParam(required = false) ProductCategory category,
+      @RequestParam(required = false) String search,
+      // 排序 Sorting
+      @RequestParam(required = false) String[] orderBy,
+      @RequestParam(required = false) String[] sort,
+      // 分頁 Pagination
+      @RequestParam(defaultValue = "5") @Min(0) @Max(1000) Integer limit,
+      @RequestParam(defaultValue = "0") @Min(0) Integer offset) {
+    ProductQueryParams queryParams = ProductQueryParams.builder()
+                                                       .category(category)
+                                                       .search(search)
+                                                       .orderBy(orderBy)
+                                                       .sort(sort)
+                                                       .limit(limit)
+                                                       .offset(offset)
+                                                       .build();
+    List<Product> list = productService.getProducts(queryParams);
+    return ResponseEntity.status(HttpStatus.OK)
+                         .body(list);
+  }
+
+  @GetMapping("/products/{productId}")
+  public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
+    Product product = productService.getProductById(productId);
+    if (product != null) {
+      return ResponseEntity.ok(product);
+    }
+    return ResponseEntity.notFound()
+                         .build();
+  }
+
+  @PostMapping("/products")
+  public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+    Integer productId = productService.createProduct(productDto);
+    Product product = productService.getProductById(productId);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+                         .body(product);
+  }
+
+  @PutMapping("/products/{productId}")
+  public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+      @RequestBody ProductDto productDto) {
+    Product product = productService.getProductById(productId);
+    if (product == null) {
+      return ResponseEntity.notFound()
+                           .build();
     }
 
-    @GetMapping("/products/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
-        Product product = productService.getProductById(productId);
-        if (product != null) {
-            return ResponseEntity.ok(product);
-        }
-        return ResponseEntity.notFound()
-                             .build();
-    }
+    productService.updateProduct(productId, productDto);
+    Product updProduct = productService.getProductById(productId);
 
-    @PostMapping("/products")
-    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
-        Integer productId = productService.createProduct(productDto);
-        Product product = productService.getProductById(productId);
+    return ResponseEntity.ok(updProduct);
+  }
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(product);
-    }
+  @DeleteMapping("/products/{productId}")
+  public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+    productService.deleteProductById(productId);
 
-    @PutMapping("/products/{productId}")
-    public  ResponseEntity<Product> updateProduct(@PathVariable Integer productId, @RequestBody ProductDto productDto) {
-        Product product = productService.getProductById(productId);
-        if (product == null){
-            return ResponseEntity.notFound()
-                                 .build();
-        }
-
-        productService.updateProduct(productId, productDto);
-        Product updProduct = productService.getProductById(productId);
-
-        return ResponseEntity.ok(updProduct);
-    }
-
-    @DeleteMapping("/products/{productId}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
-        productService.deleteProductById(productId);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                             .build();
-    }
+    return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                         .build();
+  }
 }
--- src/main/java/com/mis7ake7411/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/mis7ake7411/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,12 +1,10 @@
 package com.mis7ake7411.springbootmall.dao.impl;
 
-import com.mis7ake7411.springbootmall.constant.ProductCategory;
 import com.mis7ake7411.springbootmall.dao.ProductDao;
 import com.mis7ake7411.springbootmall.dto.ProductDto;
 import com.mis7ake7411.springbootmall.dto.ProductQueryParams;
 import com.mis7ake7411.springbootmall.model.Product;
 import com.mis7ake7411.springbootmall.rowmapper.ProductRowMapper;
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -22,106 +20,111 @@ import java.util.Map;
 @Component
 public class ProductDaoImpl implements ProductDao {
 
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  @Autowired
+  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    @Override
-    public List<Product> getProducts(ProductQueryParams queryParams) {
-        StringBuilder sql = new StringBuilder(
-                "SELECT product_id, product_name, category, image_url, " +
-                "price, stock, description, created_date, last_modified_date " +
-                "FROM product WHERE 1=1 ");
-        Map<String, Object> params = new HashMap<String, Object>();
-
-        if (queryParams.getCategory() != null) {
-            sql.append("AND category = :category ");
-            params.put("category", queryParams.getCategory().name());
+  @Override
+  public List<Product> getProducts(ProductQueryParams queryParams) {
+    StringBuilder sql = new StringBuilder(
+        "SELECT product_id, product_name, category, image_url, " +
+        "price, stock, description, created_date, last_modified_date " +
+        "FROM product WHERE 1=1 ");
+    Map<String, Object> params = new HashMap<String, Object>();
+    // 查詢條件 Filtering
+    if (queryParams.getCategory() != null) {
+      sql.append("AND category = :category ");
+      params.put("category", queryParams.getCategory().name());
+    }
+    if (queryParams.getSearch() != null) {
+      sql.append("AND UPPER(product_name) LIKE :search ");
+      params.put("search", "%" + queryParams.getSearch().toUpperCase() + "%");
+    }
+    // 排序 Sorting
+    if (queryParams.getOrderBy() != null && queryParams.getOrderBy().length > 0) {
+      sql.append(" ORDER BY ");
+      for (int i = 0; i < queryParams.getOrderBy().length; i++) {
+        sql.append(queryParams.getOrderBy()[i])
+            .append(" ")
+            .append(queryParams.getSort() != null && queryParams.getSort().length > i
+                ? queryParams.getSort()[i] : "DESC");
+        if (i < queryParams.getOrderBy().length - 1) {
+          sql.append(", ");
         }
-        if (queryParams.getSearch() != null) {
-            sql.append("AND UPPER(product_name) LIKE :search ");
-            params.put("search", "%" + queryParams.getSearch().toUpperCase() + "%");
-        }
-        if (queryParams.getOrderBy() != null && queryParams.getOrderBy().length > 0) {
-            sql.append(" ORDER BY ");
-            for (int i = 0; i < queryParams.getOrderBy().length; i++) {
-                sql.append(queryParams.getOrderBy()[i])
-                   .append(" ")
-                   .append(queryParams.getSort()[i]);
-                if (i < queryParams.getOrderBy().length - 1) {
-                    sql.append(", ");
-                }
-            }
-        }else {
-            sql.append(" ORDER BY created_date DESC");
-        }
+      }
+    } else {
+      sql.append(" ORDER BY created_date DESC");
+    }
+    // 分頁 Pagination
+    sql.append(" LIMIT :limit OFFSET :offset");
+    params.put("limit", queryParams.getLimit());
+    params.put("offset", queryParams.getOffset());
 
-        List<Product> query = namedParameterJdbcTemplate.query(sql.toString(), params, new ProductRowMapper());
-        return query;
+    return namedParameterJdbcTemplate.query(sql.toString(), params, new ProductRowMapper());
+  }
+
+  @Override
+  public Product getProductById(Integer id) {
+    String sql = "SELECT product_id, product_name, category, image_url, " +
+        "price, stock, description, created_date, last_modified_date " +
+        "FROM product WHERE product_id = :id";
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("id", id);
+
+    List<Product> query = namedParameterJdbcTemplate.query(sql, params, new ProductRowMapper());
+    if (!query.isEmpty()) {
+      return query.get(0);
     }
 
-    @Override
-    public Product getProductById(Integer id) {
-        String sql = "SELECT product_id, product_name, category, image_url, " +
-                     "price, stock, description, created_date, last_modified_date " +
-                     "FROM product WHERE product_id = :id";
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("id", id);
+    return null;
+  }
 
-        List<Product> query = namedParameterJdbcTemplate.query(sql, params, new ProductRowMapper());
-        if (!query.isEmpty()) {
-            return query.get(0);
-        }
+  @Override
+  public Integer createProduct(ProductDto productDto) {
+    String sql = "INSERT INTO product(product_name, category, image_url, " +
+        "price, stock, description, created_date, last_modified_date) " +
+        "VALUES (:productName, :category, :imageUrl, :price, :stock, " +
+        ":description, :createdDate, :lastModifiedDate)";
+    Map<String, Object> params = new HashMap<>();
+    params.put("productName", productDto.getProductName());
+    params.put("category", productDto.getCategory().toString());
+    params.put("imageUrl", productDto.getImageUrl());
+    params.put("price", productDto.getPrice());
+    params.put("stock", productDto.getStock());
+    params.put("description", productDto.getDescription());
+    Date now = new Date();
+    params.put("createdDate", now);
+    params.put("lastModifiedDate", now);
 
-        return null;
-    }
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(params), keyHolder);
 
-    @Override
-    public Integer createProduct(ProductDto productDto) {
-        String sql = "INSERT INTO product(product_name, category, image_url, " +
-                     "price, stock, description, created_date, last_modified_date) " +
-                     "VALUES (:productName, :category, :imageUrl, :price, :stock," +
-                     ":description, :createdDate, :lastModifiedDate)";
-        Map<String, Object> params = new HashMap<>();
-        params.put("productName", productDto.getProductName());
-        params.put("category", productDto.getCategory().toString());
-        params.put("imageUrl", productDto.getImageUrl());
-        params.put("price", productDto.getPrice());
-        params.put("stock", productDto.getStock());
-        params.put("description", productDto.getDescription());
-        Date now = new Date();
-        params.put("createdDate", now);
-        params.put("lastModifiedDate", now);
+    return keyHolder.getKey()
+        .intValue();
+  }
 
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(params), keyHolder);
+  @Override
+  public void updateProduct(Integer id, ProductDto productDto) {
+    String sql = "UPDATE product SET product_name =:productName, category =:category, image_url =:imageUrl, " +
+            "price =:price, stock =:stock, description =:description, last_modified_date =:lastModifiedDate " +
+            "WHERE product_id = :id";
+    Map<String, Object> params = new HashMap<>();
+    params.put("id", id);
+    params.put("productName", productDto.getProductName());
+    params.put("category", productDto.getCategory().toString());
+    params.put("imageUrl", productDto.getImageUrl());
+    params.put("price", productDto.getPrice());
+    params.put("stock", productDto.getStock());
+    params.put("description", productDto.getDescription());
+    params.put("lastModifiedDate", new Date());
+    namedParameterJdbcTemplate.update(sql, params);
+  }
 
-        return keyHolder.getKey()
-                        .intValue();
-    }
-
-    @Override
-    public void updateProduct(Integer id, ProductDto productDto) {
-        String sql = "UPDATE product SET product_name =:productName, category =:category, image_url =:imageUrl," +
-                     "price =:price, stock =:stock, description =:description, last_modified_date =:lastModifiedDate " +
-                     "WHERE product_id = :id";
-        Map<String, Object> params = new HashMap<>();
-        params.put("id", id);
-        params.put("productName", productDto.getProductName());
-        params.put("category", productDto.getCategory().toString());
-        params.put("imageUrl", productDto.getImageUrl());
-        params.put("price", productDto.getPrice());
-        params.put("stock", productDto.getStock());
-        params.put("description", productDto.getDescription());
-        params.put("lastModifiedDate", new Date());
-        namedParameterJdbcTemplate.update(sql, params);
-    }
-
-    @Override
-    public void deleteProductById(Integer id) {
-        String sql = "DELETE FROM product WHERE product_id = :id";
-        Map<String, Object> params = new HashMap<>();
-        params.put("id", id);
-        namedParameterJdbcTemplate.update(sql, params);
-    }
+  @Override
+  public void deleteProductById(Integer id) {
+    String sql = "DELETE FROM product WHERE product_id = :id";
+    Map<String, Object> params = new HashMap<>();
+    params.put("id", id);
+    namedParameterJdbcTemplate.update(sql, params);
+  }
 
 }
